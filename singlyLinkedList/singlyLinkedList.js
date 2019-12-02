142b93bfa8e501c438b6f4929479e9dbd32bdfaf
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2019-12-02 20:55:16 +0000

    create singly linked list

---
+++ singlyLinkedList/singlyLinkedList.js
@@ -0,0 +1,119 @@
+class SinglyLinkedList {
+  constructor() {
+    this.size = 0;
+    this._head = null;
+    this._tail = null;
+  }
+
+  /*
+   * Creates new node using value passed by parameter
+   * @params {*} value - node value
+   * @returns {Object}
+   */
+  _createNode(value) {
+    return {
+      value,
+      next: null,
+    };
+  }
+
+  /*
+   * Add new node at last position in the LinkedList
+   * @params {*} value - node value
+   * @returns {undefined}
+   */
+  addLast(value) {
+    const node = this._createNode(value);
+
+    if (!this._head) {
+      this._head = node;
+    } else {
+      this._tail.next = node;
+    }
+
+    this.size = this.size + 1;
+    this._tail = node;
+  }
+
+  /*
+   * Adds new node at first position in the LinkedList
+   * @params {*} value - node value
+   * @returns {undefined}
+   */
+  addFirst(value) {
+    const node = this._createNode(value);
+    this.size = this.size + 1;
+
+    if (!this._head) {
+      this._head = node;
+      this._tail = node;
+    } else {
+      node.next = this._head;
+      this._head = node;
+    }
+  }
+
+  /*
+   * Returns value of node at index position passed by parameter
+   * @params {Number} index - node index
+   * @returns {*}
+   */
+  findByIndex(index) {
+    if (index < 0 || index >= this.size) {
+      return null;
+    }
+
+    let current = this._head;
+    let i = 0;
+
+    while (i < index) {
+      i = i + 1;
+      current = current.next;
+    }
+
+    return current.value;
+  }
+
+  /*
+   * Remove by index
+   * @params {Number} index - node index
+   * @returns {Boolean}
+   */
+  removeByIndex(index) {
+    if (index < 0 || index >= this.size || !this._head) {
+      return false;
+    }
+
+    if (index === 0) {
+      this._head = this._head.next;
+
+      if (!this._head) {
+        this._tail = null;
+      }
+    } else if (index === this.size - 1) {
+      let prev = this._head;
+
+      while (prev.next !== this._tail) {
+        prev = prev.next;
+      }
+
+      this._tail = prev;
+      this._tail.next = null;
+    } else {
+      let current = this._head;
+      let i = 0;
+
+      while (i < index - 1) {
+        i = i + 1;
+        current = current.next;
+      }
+
+      current.next = current.next.next;
+    }
+
+    this.size = this.size - 1;
+    return true;
+  }
+}
+
+module.exports = SinglyLinkedList;
---
+++ singlyLinkedList/test.js
@@ -0,0 +1,59 @@
+const SinglyLinkedList = require('./singlyLinkedList');
+
+describe('singly linked list', () => {
+  it('should add new node at end of list', () => {
+    const linkedList = new SinglyLinkedList();
+    linkedList.addLast('javascript');
+    linkedList.addLast('lua');
+    linkedList.addLast('ruby');
+
+    expect(linkedList.size).toBe(3);
+    expect(linkedList.findByIndex(0)).toBe('javascript');
+    expect(linkedList.findByIndex(1)).toBe('lua');
+    expect(linkedList.findByIndex(2)).toBe('ruby');
+  });
+
+  it('should add new node at beginning of list', () => {
+    const linkedList = new SinglyLinkedList();
+    linkedList.addFirst('javascript');
+    linkedList.addFirst('lua');
+    linkedList.addFirst('ruby');
+
+    expect(linkedList.size).toBe(3);
+    expect(linkedList.findByIndex(2)).toBe('javascript');
+    expect(linkedList.findByIndex(1)).toBe('lua');
+    expect(linkedList.findByIndex(0)).toBe('ruby');
+  });
+
+  it('should return false in case node was not found', () => {
+    const linkedList = new SinglyLinkedList();
+
+    expect(linkedList.removeByIndex(-1)).toBeFalsy();
+    expect(linkedList.removeByIndex(0)).toBeFalsy();
+
+    linkedList.addLast('javascript');
+    expect(linkedList.removeByIndex(1)).toBeFalsy();
+  });
+
+  it('should return true in case node was found', () => {
+    const linkedList = new SinglyLinkedList();
+    linkedList.addFirst('javascript');
+    expect(linkedList.removeByIndex(0)).toBeTruthy();
+    expect(linkedList.size).toBe(0);
+    expect(linkedList.findByIndex(0)).toBeNull();
+
+    linkedList.addLast('javascript');
+    linkedList.addLast('ruby');
+    linkedList.addLast('python');
+    expect(linkedList.removeByIndex(2)).toBeTruthy();
+    expect(linkedList.size).toBe(2);
+
+    linkedList.addLast('typescript');
+    linkedList.addLast('c');
+    expect(linkedList.removeByIndex(2)).toBeTruthy();
+    expect(linkedList.size).toBe(3);
+    expect(linkedList.findByIndex(0)).toBe('javascript');
+    expect(linkedList.findByIndex(1)).toBe('ruby');
+    expect(linkedList.findByIndex(2)).toBe('c');
+  });
+});
