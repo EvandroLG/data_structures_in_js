39013942c189394b48830c4986e35058c0aca985
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2020-06-01 00:10:45 +0200

    add comments in quickUnion

--- quickUnion/QuickUnion.js
+++ quickUnion/QuickUnion.js
@@ -1,17 +1,34 @@
 const QuickFind = size => {
   const elements = [...Array(size).keys()];
 
+  /*
+   * Given i, find the root
+   * @params {number} i - index
+   * @returns {number}
+   */
   const root = i => {
-    let _root = i;
-    if (_root !== elements[_root]) _root = root(_root);
-    return _root;
+    let index = i;
+    if (index !== elements[index]) index = root(index);
+    return index;
   };
 
   return {
+    /*
+     * Check if elements are connected
+     * @params {number} x
+     * @params {number} y
+     * @returns {boolean}
+     */
     isConnected(x, y) {
       return root(elements[x]) === root(elements[y]);
     },
 
+    /*
+     * Create union
+     * @params {number} x
+     * @params {number} y
+     * @returns {undefined}
+     */
     union(x, y) {
       elements[root(x)] = root(y);
     },
